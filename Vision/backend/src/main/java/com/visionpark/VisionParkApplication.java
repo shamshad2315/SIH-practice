429f0d1cbf39ec2c62fd234a0ee2103de6262d14
Shahil Ahamad <228204771+shahilweb@users.noreply.github.com>
2026-08-20 01:03:33 +0530

    Add files via upload

---
+++ Vision/backend/src/main/java/com/visionpark/VisionParkApplication.java
@@ -0,0 +1,12 @@
+package com.visionpark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisionParkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VisionParkApplication.class, args);
+    }
+}
